a957d006512ea9ab3525c76914c36a47d3014ba2
Willy Weiskopf <william@weiskopf.me>
2013-09-27 18:19:08 -0600

    Continued the methods

--- CluePath/src/clue/Cell.java
+++ CluePath/src/clue/Cell.java
@@ -1,5 +1,23 @@
 package clue;
 
 public class Cell {
+	public Cell() {
+		
+	}
 	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void startTargets() {
+		
+	}
+	
+	public getTargets() {
+		
+	}
+	
+	public getAdjList() {
+		
+	}
 }
