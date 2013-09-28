1a79a5d2ee7afa26674d39344bc4748ec94afb5f
george <rpubaddr0@gmail.com>
2013-09-27 18:10:51 -0600

    Added blank failing tests.

---
+++ CluePath/src/test/TestAdjacencyList.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdjacencyList {
+
+	@Test
+	public void testAdjacencyListMiddle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListRightEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListLeftEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListTopEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListBottomEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListTopRightCorner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListTopLeftCorner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListBottomRightCorner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdjacencyListBottomLeftCorner() {
+		fail("Not yet implemented");
+	}
+}
