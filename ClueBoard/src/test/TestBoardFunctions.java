a3d919d10a56127b07316fd96de44cc260b3f65b
george <rpubaddr0@gmail.com>
2013-10-02 21:19:19 -0600

    Created failing tests.

---
+++ ClueBoard/src/test/TestBoardFunctions.java
@@ -0,0 +1,97 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBoardFunctions {
+	Board board;
+	RoomCell room;
+	int rows;
+	int columns;
+
+	@Before
+	public void setUp() {
+		board = new Board();
+		rows = 23;
+		columns = 23;
+	}
+
+	@Test
+	public void testRoomsMapNumberOfRooms() {
+		int numRooms = 10;
+		assertEquals(numRooms, board.getRooms().size());
+	}
+
+	@Test
+	public void testMappingWorks() {
+		String roomName = "Laboratory";
+		char roomInitial = 'Y';
+		assertEquals(roomName, board.getRooms().get(roomInitial));
+		
+		roomName = "Closet";
+		roomInitial = 'X';
+		assertEquals(roomName, board.getRooms().get(roomInitial));
+		
+		roomName = "Library";
+		roomInitial = 'I';
+		assertEquals(roomName, board.getRooms().get(roomInitial));
+	}
+
+	@Test
+	public void testCorrectNumberOfRowsAndColumns() {
+		assertEquals(rows, board.getNumRows());
+		assertEquals(columns, board.getNumColumns());
+	}
+
+	@Test
+	public void testDoorHasCorrectDirection() {
+		room = board.getRoomCellAt(6, 1);
+		assertTrue(room.isDoorway());
+		assertEquals(RoomCell.DoorDirection.DOWN, room.getDoorDirection());
+
+		room = board.getRoomCellAt(0, 11);
+		assertTrue(room.isDoorway());
+		assertEquals(RoomCell.DoorDirection.LEFT, room.getDoorDirection());
+
+		room = board.getRoomCellAt(15, 13);
+		assertTrue(room.isDoorway());
+		assertEquals(RoomCell.DoorDirection.RIGHT, room.getDoorDirection());
+
+		room = board.getRoomCellAt(15, 8);
+		assertTrue(room.isDoorway());
+		assertEquals(RoomCell.DoorDirection.UP, room.getDoorDirection());
+
+		room = board.getRoomCellAt(10, 10);
+		assertTrue(room.isDoorway());
+		assertEquals(RoomCell.DoorDirection.NONE, room.getDoorDirection());
+	}
+
+	@Test
+	public void testRoomAssignment() {
+		char roomInitial = 'F';
+		assertEquals(roomInitial, board.getRoomCellAt(7, 5).getInitial());
+		
+		roomInitial = 'S';
+		assertEquals(roomInitial, board.getRoomCellAt(12, 3).getInitial());
+		
+		roomInitial = 'H';
+		assertEquals(roomInitial, board.getRoomCellAt(9, 20).getInitial());
+	}
+
+	@Test
+	public void testCalcIndex() {
+		assertEquals(0, board.calcIndex(0, 0));
+		assertEquals(528, board.calcIndex(rows-1, columns-1));
+		assertEquals(342, board.calcIndex(14, 20));
+	}
+
+	@Test (expected = BadConfigFormatException.class)
+	public void testBadConfigFormatException() throws BadConfigFormatException, FileNotFoundException {
+		// board has bad config files!
+		board.loadConfigFiles();
+	}
+}
