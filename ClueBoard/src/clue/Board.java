69c970d1571406a00033514504286c52314db37a
William Weiskopf <william@weiskopf.me>
2013-10-04 22:45:08 -0600

    Board class loads csv into array

--- ClueBoard/src/clue/Board.java
+++ ClueBoard/src/clue/Board.java
@@ -1,16 +1,26 @@
 package clue;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Board {
 	private ArrayList<BoardCell> cells;
 	private Map<Character,String> rooms;
 	private int numRows;
 	private int numCols;
+	private String [] config;
 	
-	public void loadConfigFiles() {
-		
+	public void loadConfigFiles() throws FileNotFoundException {
+		FileReader reader = new FileReader("ClueBoard.csv");
+		Scanner in = new Scanner(reader);
+		String configString = "";
+		while(in.hasNextLine()) {
+			configString += in.nextLine();
+		}
+		this.config = configString.split(",");
 	}
 	
 	public int calcIndex(int row, int col) {
@@ -33,4 +43,13 @@ public class Board {
 		return numCols;
 	}
 
+	public static void main(String[] args) {
+		Board board = new Board();
+		try {
+			board.loadConfigFiles();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
