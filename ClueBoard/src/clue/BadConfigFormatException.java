a6ddb55377c08aba1ba3d2a3b143dcfabd15cf5d
george <rpubaddr0@gmail.com>
2013-10-05 17:14:34 -0600

    Added log file writing to the exception class.

--- ClueBoard/src/clue/BadConfigFormatException.java
+++ ClueBoard/src/clue/BadConfigFormatException.java
@@ -1,7 +1,20 @@
 package clue;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BadConfigFormatException extends Exception {
+	private String logfile = "error.log";
+
 	public BadConfigFormatException(String message) {
 		super(message);
+
+		try {
+			FileWriter log = new FileWriter(logfile, true);
+			log.write(this.toString());
+			log.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
