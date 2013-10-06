6ee601c0a947ff88e84ba44abbd0419794fbf1b6
george <rpubaddr0@gmail.com>
2013-10-05 18:26:57 -0600

    Added setUp for tests.

--- ClueBoard/src/test/TestBoardAdjacencies.java
+++ ClueBoard/src/test/TestBoardAdjacencies.java
@@ -2,13 +2,30 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestBoardAdjacencies {
+import clue.BadConfigFormatException;
+import clue.Board;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class TestBoardAdjacencies {
+	private static Board board;
+
+	@Before
+	public static void setUp() {
+		board = new Board();
+		try {
+			board.loadConfigFiles("ClueBoard.csv", "legend.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (BadConfigFormatException e) {
+			e.printStackTrace();
+		}
+		board.calcAdjacencies();
 	}
+	
+	
 
 }
